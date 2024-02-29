68be673429a6b5aafee6243afb8b26af79194556
Sanieeme <shoneesani@gmail.com>
2024-02-29 05:01:06 -0800

    right insert correct

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (node == NULL || parent == NULL)
 	{
-		return;
+		return (NULL);
 	}
 	node->n = value;
 	node->parent = parent;
