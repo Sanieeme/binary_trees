7a456ff7ff7b1bd5ecf7b24aacdd6ec08b07d776
Sanieeme <shoneesani@gmail.com>
2024-02-29 05:36:03 -0800

    insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = malloc(sizeof(binary_tree_t));
+	node = binary_tree_node(parent, value);
 
 	if (node == NULL || parent == NULL)
 	{
