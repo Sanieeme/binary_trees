f3100945b073ae3eefb634c021da20d9ab679bd3
Sanieeme <shoneesani@gmail.com>
2024-02-29 05:42:32 -0800

    s

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,9 +11,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (parent == NULL)
+		return (NULL);
 	node = binary_tree_node(parent, value);
 
-	if (node == NULL || parent == NULL)
+	if (node == NULL)
 	{
 		return (NULL);
 	}
