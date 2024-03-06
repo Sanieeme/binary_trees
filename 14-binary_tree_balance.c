8a8d67dfbdad2612ae561e21486dd7b412ba395e
Sanieeme <shoneesani@gmail.com>
2024-03-05 19:48:35 -0800

    s

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -27,4 +27,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	lheight = binary_tree_height(tree->left);
 	rheight = binary_tree_height(tree->right);
 
-	return (1 + (lheight > rheight ? lheight : rheight));}
+	return (1 + (lheight > rheight ? lheight : rheight));
+}
